d8bb02edcb01d91952c6438c019672fcbaa643a3
Andrew Horlanov <andrew.horlanov@gmail.com>
2023-08-23 11:36:07 +0200

    Dark mode work, header changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import React from "react";
 import Projects from "./styles/components/Projects/projects";
 import Footer from "./styles/components/Footer/Footer";
@@ -7,11 +6,28 @@ import About from "./styles/components/About/about";
 import Features from "./styles/components/Features/features";
 import MobileNavBar from "./styles/components/NavbarMobile/mobilenavbar";
 import Profile from "./styles/components/Profile/profile";
+import MyHead from "./styles/components/Header/head";
+import Header from "./styles/components/Header/header";
+import Contact from "./styles/components/Contact/contact";
 
 const Home: React.FC = () => {
   return (
     <div>
-      <Profile />
+      <Head>
+        <MyHead />
+      </Head>
+      <body>
+        <Header />
+        <MobileNavBar />
+        <main>
+          <Profile />
+          <About />
+          <Features />
+          <Projects />
+          <Contact />
+          <Footer />
+        </main>
+      </body>
     </div>
   );
 };
