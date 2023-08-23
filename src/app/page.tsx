cf772bb7123c3b6c8e38ee92d774ca6326586657
Andrew Horlanov <andrew.horlanov@gmail.com>
2023-08-23 09:01:25 +0200

    About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import Head from "next/head";
 import Image from "next/image";
 import React from "react";
 import Projects from "./styles/components/Projects/projects";
+import Features from "./styles/components/Contact/contact";
+import About from "./styles/components/About/about";
 
 const Home: React.FC = () => {
-  return (
-    <div>
-      <Projects />;
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
--- src/app/styles/components/About/about.tsx
+++ src/app/styles/components/About/about.tsx
@@ -1,51 +1,68 @@
 import React from "react";
+import styles from "./about.module.css";
+import globalStyles from "@/app/utils.module.css";
+import lazyLoading from "@/app/utils/lazy-loading";
+import Image from "next/image";
 
-const MobileNavBar: React.FC = () => {
+const About: React.FC = () => {
+  const lazyLoadingClass = lazyLoading();
+  const aspectRatioWidth = 900;
+  const aspectRatioHeight = (9 / 16) * aspectRatioWidth;
   return (
-    <section id="about" className="about container section">
-      <div className="about__content">
-        <h2 className="about__title">About</h2>
-        <p className="about__description">
-          Hello there! I'm Ramzi, a
-          <strong>Frontend / Fullstack-Jamstack</strong> web developer based
-          near Montreal. I wake up in the morning, grab a cup of timmies coffee,
-          open up my laptop and build things that live on the web all day,
-          everyday.
-        </p>
-        <p className="about__description">
-          People who know me say I'm obsessed, that's fair, I am very driven and
-          love learning new things.
-        </p>
-        <p className="about__description">
-          I recently created a YouTube channel called
-          <a>Slaying The Dragon</a> where I upload videos on front-end web
-          development topics.
-        </p>
-        <hr className="about__hr" />
-        <h3 className="about__subtitle">Technologies</h3>
-        <div className="about__ul-container">
-          <ul className="about__ul">
-            <li className="about__list">JavaScript(ES6+)/Typescript</li>
-            <li className="about__list">CSS</li>
-            <li className="about__list">HTML</li>
-          </ul>
-          <ul className="about__ul">
-            <li className="about__list">ReactJS</li>
-            <li className="about__list">NextJS</li>
-            <li className="about__list">TailwindCSS</li>
-          </ul>
+    <section id="about" className={globalStyles.section}>
+      <div className={globalStyles.container}>
+        <div className={styles.about}>
+          <div className={styles.about__content}>
+            <h2 className={styles.about__title}>About</h2>
+            <p className={styles.about__description}>
+              Hello there! I'm Ramzi, a
+              <strong>Frontend / Fullstack-Jamstack</strong> web developer based
+              near Montreal. I wake up in the morning, grab a cup of timmies
+              coffee, open up my laptop and build things that live on the web
+              all day, everyday.
+            </p>
+            <p className={styles.about__description}>
+              People who know me say I'm obsessed, that's fair, I am very driven
+              and love learning new things.
+            </p>
+            <p className={styles.about__description}>
+              I recently created a YouTube channel called
+              <a>Slaying The Dragon</a> where I upload videos on front-end web
+              development topics.
+            </p>
+            <hr className={styles.about__hr} />
+            <h3 className={styles.about__subtitle}>Technologies</h3>
+            <div className={styles.about__ul_container}>
+              <ul className={styles.about__ul}>
+                <li className={styles.about__list}>
+                  JavaScript(ES6+)/Typescript
+                </li>
+                <li className={styles.about__list}>CSS</li>
+                <li className={styles.about__list}>HTML</li>
+              </ul>
+              <ul className={styles.about__ul}>
+                <li className={styles.about__list}>ReactJS</li>
+                <li className={styles.about__list}>NextJS</li>
+                <li className={styles.about__list}>TailwindCSS</li>
+              </ul>
+            </div>
+          </div>
+          <div className={styles.about__img_wrapper}>
+            <div className={styles.about__img}>
+              <Image
+                className={`my-element ${lazyLoadingClass}`}
+                src="https://via.placeholder.com/720"
+                data-src="/about.jpg"
+                alt="Profile picture"
+                width={aspectRatioWidth}
+                height={aspectRatioHeight}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="about__img-wrapper">
-        <img
-          className="about__img lazy loading"
-          src="https://via.placeholder.com/720"
-          data-src="/about.jpg"
-          alt="Profile picture"
-        />
       </div>
     </section>
   );
 };
 
-export default MobileNavBar;
+export default About;
