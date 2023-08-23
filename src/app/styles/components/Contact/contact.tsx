370095c2bf614d9e83ae557b2c424d577a456b92
Andrew Horlanov <andrew.horlanov@gmail.com>
2023-08-23 08:45:50 +0200

    Contact update

--- src/app/styles/components/Contact/contact.tsx
+++ src/app/styles/components/Contact/contact.tsx
@@ -1,21 +1,31 @@
 import React from "react";
+import styles from "./contact.module.css";
+import globalStyles from "@/app/utils.module.css";
 
 const Features: React.FC = () => {
   return (
-    <section id="contact" className="contact container section">
-      <h2 className="contact__title">Get In Contact</h2>
-      <p className="contact__description">
-        Whether you are starting a project, have business inquiries or just want
-        to say hi, my inbox is always open so feel free to reach out and I will
-        get back to you as soon as possible.
-      </p>
-      <a
-        className="contact__btn btn"
-        href="mailto:slayingthedragon.io@gmail.com"
-        target="_blank"
-      >
-        React Out
-      </a>
+    <section id="contact" className={globalStyles.container}>
+      <div className={globalStyles.section}>
+        <div className={styles.contact}>
+          <h2 className={styles.contact__title}>Get In Contact</h2>
+          <p className={styles.contact__description}>
+            Whether you are starting a project, have business inquiries or just
+            want to say hi, my inbox is always open so feel free to reach out
+            and I will get back to you as soon as possible.
+          </p>
+          <div className={styles.contact__btn}>
+            <a
+              className={globalStyles.btn}
+              href="mailto:andrew.horlanov@gmail.com"
+              target="_blank"
+            >
+              React Out
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
+
+export default Features;
