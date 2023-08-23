07755adba3649525e827d95d5f50e7b8b959d02e
Andrew Horlanov <andrew.horlanov@gmail.com>
2023-08-23 16:59:19 +0200

    Some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,30 +4,22 @@ import Projects from "./styles/components/Projects/projects";
 import Footer from "./styles/components/Footer/Footer";
 import About from "./styles/components/About/about";
 import Features from "./styles/components/Features/features";
-import MobileNavBar from "./styles/components/NavbarMobile/mobilenavbar";
 import Profile from "./styles/components/Profile/profile";
-import MyHead from "./styles/components/Header/head";
 import Header from "./styles/components/Header/header";
 import Contact from "./styles/components/Contact/contact";
 
 const Home: React.FC = () => {
   return (
     <div>
-      <Head>
-        <MyHead />
-      </Head>
-      <body>
-        <Header />
-        <MobileNavBar />
-        <main>
-          <Profile />
-          <About />
-          <Features />
-          <Projects />
-          <Contact />
-          <Footer />
-        </main>
-      </body>
+      <Header />
+      <main>
+        <Profile />
+        <About />
+        <Features />
+        <Projects />
+        <Contact />
+        <Footer />
+      </main>
     </div>
   );
 };
--- src/app/styles/components/Header/header.tsx
+++ src/app/styles/components/Header/header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect } from "react";
-
 import styles from "./header.module.css";
 import globalStyles from "@/app/utils.module.css";
 import darkMode from "@/app/utils/dark-mode";
@@ -9,6 +8,7 @@ const Header: React.FC = () => {
   useEffect(() => {
     darkMode(); // Call the darkMode function when the component mounts
   }, []);
+
   return (
     <header className={globalStyles.container}>
       <div className={styles.header}>
@@ -31,11 +31,7 @@ const Header: React.FC = () => {
             </li>
             <li className={styles.header__line}></li>
             <li>
-              <button
-                aria-label="theme_toggle btn"
-                id="theme_toggle"
-                className={styles.header__sun}
-              >
+              <button id="theme_toggle" className={styles.header__sun}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -52,22 +48,6 @@ const Header: React.FC = () => {
               </a>
             </li>
           </ul>
-          <button
-            aria-label="mobile nav button"
-            className={styles.header__bars}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </button>
         </nav>
       </div>
     </header>
